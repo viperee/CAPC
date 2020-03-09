c1490980f86d6abae6a5794eaa6fbb7ac2a713a8
viperee <just.b.nd.l@gmail.com>
2020-03-10 00:36:00 +0100

    Style sync

--- head.php
+++ head.php
@@ -5,5 +5,4 @@
 <meta name="referrer" content="origin">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <link rel="icon" type="image/png" href="assets/media/img/favicon.png">
-<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">ddddd
+<link rel="stylesheet" type="text/css" href="css/assets/scss/footer.css">
