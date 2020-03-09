ab95278ddeb8a91f5df61ce92736bd9aa19bc3c0
viperee <just.b.nd.l@gmail.com>
2020-03-09 23:36:47 +0100

    footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,52 @@
 <footer>
-    
+    <div><!--HAUT-->
+        <div><!--LOGO-->
+            <img src="assets/media/img/Desktop/Footer/Logo.svg" />
+        </div>
+        <div><!--RSX SOCIAUX-->
+            <img src="assets/media/img/Desktop/Footer/Icones/Reseaux%2520Sociaux/Facebook.svg" />
+            <img src="assets/media/img/Desktop/Footer/Icones/Reseaux%2520Sociaux/Twitter.svg" />
+            <img src="assets/media/img/Desktop/Footer/Icones/Reseaux%2520Sociaux/Instagram.svg" />
+        </div>
+    </div>
+    <div><!--MILLIEU-->
+        <div><!--LIENS-->
+            <a href="index.php">Accueil</a>
+            <a href="programmation.php">Programmation</a>
+            <a href="musee.php">Le musée</a>
+            <a href="collections.php">Collections</a>
+            <a href="infos-pratiques.php">Infos Pratiques</a>
+            <a href="mentions.php">Mentions légales</a>
+        </div>
+        <div><!--HORAIRES-->
+            <span class="titre-footer">Horaires d'ouverture</span>
+            <p>11 h – 18 h
+                Fermeture tardive le 2e mercredi de chaque mois, jusqu’à 20 h
+                Fermé les lundis et jours fériés</p>
+        </div>
+        <div><!--CONTACT-->
+            <span class="titre-footer">Contact</span>
+            <p>7, rue Ferrère
+                33000 Bordeaux, France
+
+                Tél. +33 (0)5 56 00 81 50
+                <a href="mailto:capc@mairie-bordeaux.fr">capc@mairie-bordeaux.fr</a></p>
+        </div>
+        <div><!--RECHERCHE-->
+            <label for="site-search" class="titre-footer">Recherche</label>
+            <input type="search" id="site-search" placeholder="Entrez un mot clé">
+
+            <a href="assets/media/img/Desktop/Footer/Icones/Recherche.svg"><button></button></a>
+        </div>
+    </div>
+    <div><!--MECENES-->
+        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne1.svg" />
+        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne2.svg" />
+        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne3.svg" />
+        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne4.svg" />
+        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne5.svg" />
+    </div>
+    <div><!--Copyright-->
+        <p>Designed by Victoria Pereira</p>
+    </div>
 </footer>
