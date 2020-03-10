5638d13d583486a79146d8051736cb0c4756e10e
viperee <just.b.nd.l@gmail.com>
2020-03-10 15:34:29 +0100

    Mise en place d'un fichier responsif

--- footer.php
+++ footer.php
@@ -4,27 +4,32 @@
             <img src="assets/media/img/Desktop/Footer/Logo.svg" />
         </div>
         <div><!--RSX SOCIAUX-->
-            <img src="assets/media/img/Desktop/Footer/Icones/Reseaux%2520Sociaux/Facebook.svg" />
-            <img src="assets/media/img/Desktop/Footer/Icones/Reseaux%2520Sociaux/Twitter.svg" />
-            <img src="assets/media/img/Desktop/Footer/Icones/Reseaux%2520Sociaux/Instagram.svg" />
+            <ul>
+                <li><img src="assets/media/img/Desktop/Footer/Icones/reseaux_sociaux/Facebook.svg" /></li>
+                <li><img src="assets/media/img/Desktop/Footer/Icones/reseaux_sociaux/Twitter.svg" /></li>
+                <li><img src="assets/media/img/Desktop/Footer/Icones/reseaux_sociaux/Instagram.svg" /></li>
+            </ul>
         </div>
     </div>
-    <div><!--MILLIEU-->
-        <div><!--LIENS-->
-            <a href="index.php">Accueil</a>
-            <a href="programmation.php">Programmation</a>
-            <a href="musee.php">Le musée</a>
-            <a href="collections.php">Collections</a>
-            <a href="infos-pratiques.php">Infos Pratiques</a>
-            <a href="mentions.php">Mentions légales</a>
+    <hr>
+    <div class="millieu"><!--MILLIEU-->
+        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--LIENS-->
+            <ul>
+                <li><a href="index.php">Accueil</a>
+                <li><a href="programmation.php">Programmation</a></li>
+                <li><a href="musee.php">Le musée</a></li>
+                <li><a href="collections.php">Collections</a></li>
+                <li><a href="infos-pratiques.php">Infos Pratiques</a></li>
+                <li><a href="mentions.php">Mentions légales</a></li>
+            </ul>
         </div>
-        <div><!--HORAIRES-->
+        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--HORAIRES-->
             <span class="titre-footer">Horaires d'ouverture</span>
             <p>11 h – 18 h
                 Fermeture tardive le 2e mercredi de chaque mois, jusqu’à 20 h
                 Fermé les lundis et jours fériés</p>
         </div>
-        <div><!--CONTACT-->
+        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--CONTACT-->
             <span class="titre-footer">Contact</span>
             <p>7, rue Ferrère
                 33000 Bordeaux, France
@@ -32,21 +37,25 @@
                 Tél. +33 (0)5 56 00 81 50
                 <a href="mailto:capc@mairie-bordeaux.fr">capc@mairie-bordeaux.fr</a></p>
         </div>
-        <div><!--RECHERCHE-->
-            <label for="site-search" class="titre-footer">Recherche</label>
-            <input type="search" id="site-search" placeholder="Entrez un mot clé">
-
-            <a href="assets/media/img/Desktop/Footer/Icones/Recherche.svg"><button></button></a>
+        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--RECHERCHE-->
+            <form>
+                <label for="site-search" class="titre-footer">Recherche</label></br>
+                <input type="search" id="site-search" placeholder="Entrez un mot clé">
+                <button></button>
+            </form>
         </div>
     </div>
-    <div><!--MECENES-->
-        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne1.svg" />
-        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne2.svg" />
-        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne3.svg" />
-        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne4.svg" />
-        <img src="assets/media/img/Desktop/Footer/M%25C3%25A9c%25C3%25A8nes/M%C3%A9c%C3%A8ne5.svg" />
+    <hr>
+    <div class="mecenes"><!--MECENES-->
+        <ul>
+            <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene1.svg" /></li>
+            <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene2.svg" /></li>
+            <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene3.svg" /></li>
+            <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene4.svg" /></li>
+            <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene5.svg" /></li>
+        </ul>
     </div>
-    <div><!--Copyright-->
+    <div class="copyright"><!--Copyright-->
         <p>Designed by Victoria Pereira</p>
     </div>
 </footer>
