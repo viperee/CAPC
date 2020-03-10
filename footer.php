6cef35d9ed947532f9f2222c6b9e4158781af5c9
viperee <just.b.nd.l@gmail.com>
2020-03-10 16:11:17 +0100

    flex footer

--- footer.php
+++ footer.php
@@ -25,29 +25,29 @@
         </div>
         <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--HORAIRES-->
             <span class="titre-footer">Horaires d'ouverture</span>
-            <p>11 h – 18 h
-                Fermeture tardive le 2e mercredi de chaque mois, jusqu’à 20 h
+            <p>11 h – 18 h<br />
+                Fermeture tardive le 2e mercredi de chaque mois, jusqu’à 20 h<br />
                 Fermé les lundis et jours fériés</p>
         </div>
         <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--CONTACT-->
             <span class="titre-footer">Contact</span>
-            <p>7, rue Ferrère
-                33000 Bordeaux, France
-
-                Tél. +33 (0)5 56 00 81 50
+            <p>7, rue Ferrère<br />
+                33000 Bordeaux, France<br />
+                <br />
+                Tél. +33 (0)5 56 00 81 50<br />
                 <a href="mailto:capc@mairie-bordeaux.fr">capc@mairie-bordeaux.fr</a></p>
         </div>
         <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--RECHERCHE-->
             <form>
-                <label for="site-search" class="titre-footer">Recherche</label></br>
+                <label for="site-search" class="titre-footer">Recherche</label><br />
                 <input type="search" id="site-search" placeholder="Entrez un mot clé">
                 <button></button>
             </form>
         </div>
     </div>
     <hr>
-    <div class="mecenes"><!--MECENES-->
-        <ul>
+    <div><!--MECENES-->
+        <ul class="liste-mecenes">
             <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene1.svg" /></li>
             <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene2.svg" /></li>
             <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene3.svg" /></li>
