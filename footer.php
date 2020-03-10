f186cc07c1a6e7ffefb336edcd9942c4f3598730
viperee <just.b.nd.l@gmail.com>
2020-03-10 17:44:10 +0100

    Titres maj

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div><!--LOGO-->
             <img src="assets/media/img/Desktop/Footer/Logo.svg" />
         </div>
-        <div class="xLarge-12 large-12 medium-12 small-12 xSmall-12"><!--RSX SOCIAUX-->
+        <div class="xLarge-12 large-12 medium-12 small-12"><!--RSX SOCIAUX-->
             <ul class="reseaux">
                 <li><img src="assets/media/img/Desktop/Footer/Icones/reseaux_sociaux/Facebook.svg" /></li>
                 <li><img src="assets/media/img/Desktop/Footer/Icones/reseaux_sociaux/Twitter.svg" /></li>
@@ -13,7 +13,7 @@
     </div>
     <hr>
     <div class="millieu"><!--MILLIEU-->
-        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--LIENS-->
+        <div class="xLarge-3 large-3 medium-3 small-3 liens-menu-footer"><!--LIENS-->
             <ul>
                 <li><a href="index.php">Accueil</a>
                 <li><a href="programmation.php">Programmation</a></li>
@@ -23,13 +23,13 @@
                 <li><a href="mentions.php">Mentions légales</a></li>
             </ul>
         </div>
-        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--HORAIRES-->
+        <div class="xLarge-3 large-3 medium-3 small-3"><!--HORAIRES-->
             <span class="titre-footer">Horaires d'ouverture</span>
             <p>11 h – 18 h<br />
                 Fermeture tardive le 2e mercredi de chaque mois, jusqu’à 20 h<br />
                 Fermé les lundis et jours fériés</p>
         </div>
-        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--CONTACT-->
+        <div class="xLarge-3 large-3 medium-3 small-3"><!--CONTACT-->
             <span class="titre-footer">Contact</span>
             <p>7, rue Ferrère<br />
                 33000 Bordeaux, France<br />
@@ -37,7 +37,7 @@
                 Tél. +33 (0)5 56 00 81 50<br />
                 <a href="mailto:capc@mairie-bordeaux.fr">capc@mairie-bordeaux.fr</a></p>
         </div>
-        <div class="xLarge-3 large-3 medium-3 small-3 xSmall-3"><!--RECHERCHE-->
+        <div class="xLarge-3 large-3 medium-3 small-3"><!--RECHERCHE-->
             <form>
                 <label for="site-search" class="titre-footer">Recherche</label><br />
                 <input type="search" id="site-search" placeholder="Entrez un mot clé">
@@ -46,7 +46,7 @@
         </div>
     </div>
     <hr>
-    <div class="xLarge-12 large-12 medium-12 small-12 xSmall-12"><!--MECENES-->
+    <div class="xLarge-12 large-12 medium-12 small-12"><!--MECENES-->
         <ul class="liste-mecenes">
             <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene1.svg" /></li>
             <li><img src="assets/media/img/Desktop/Footer/mecenes/mecene2.svg" /></li>
