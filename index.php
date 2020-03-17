567ee2d7f217b4f88dd2d4b054f14d39f6b07b58
viperee <just.b.nd.l@gmail.com>
2020-03-17 17:05:18 +0100

    accueil slider collections

--- index.php
+++ index.php
@@ -1,36 +1,50 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
 <head>
-    <title>TITRE</title>
-    <?php include("head.php"); ?>
     <!-- Link Swiper's CSS -->
     <link rel="stylesheet" href="assets/css/swiper.min.css">
+    <title>TITRE</title>
+    <?php include("head.php"); ?>
 </head>
 <body>
     <?php include("header.php"); ?>
 
-    <main>
+    <main class="wrapper">
         <!-- Swiper -->
         <div class="swiper-container">
             <div class="swiper-wrapper">
-            <div class="swiper-slide"><img src="assets/media/img/Home.svg"/></div>
-            <div class="swiper-slide">Slide 2</div>
-            <div class="swiper-slide">Slide 3</div>
-            <div class="swiper-slide">Slide 4</div>
-            <div class="swiper-slide">Slide 5</div>
-            <div class="swiper-slide">Slide 6</div>
-            <div class="swiper-slide">Slide 7</div>
-            <div class="swiper-slide">Slide 8</div>
-            <div class="swiper-slide">Slide 9</div>
-            <div class="swiper-slide">Slide 10</div>
+                <div class="swiper-slide">
+                    <img src="assets/media/img/Home2.svg"/><br />
+                    <div class="cat-legende">
+                        <span class="cat-periode">Exposition</span><br />
+                        <span class="legende">Histoire de l’art cherche personnages …</span><br />
+                        <span class="cat-periode">20 juin 2019 - 22 mars 2020</span>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <img src="assets/media/img/Home.svg"/><br />
+                    <div class="cat-legende">
+                        <span class="cat-periode">Visite</span><br />
+                        <span class="legende">Visite Flash</span><br />
+                        <span class="cat-periode">25 fev. 2020 - 06 mars 2020</span>
+                    </div>
+                </div>
             </div>
             <!-- Add Arrows -->
             <div class="swiper-button-next"></div>
             <div class="swiper-button-prev"></div>
         </div>
 
-
-
+        <div id="collections">
+            <img src="assets/media/img/grille-collections.svg" class="grille" />
+            <img src="assets/media/img/collections.svg" class="coll-img" />
+            <div class="card">
+                <div class="titre">
+                    <H1>Collections</H1>
+                    
+                </div>
+            </div>
+		</div>
     </main>
 
 
