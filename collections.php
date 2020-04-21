cd2042f798f732c5986e2b64859bc20442cbfedd
viperee <just.b.nd.l@gmail.com>
2020-04-21 18:16:02 +0200

    Collections in progress

--- collections.php
+++ collections.php
@@ -9,109 +9,90 @@
     <link rel="stylesheet" href="assets/css/collections.css">
 </head>
 <body>
-    <?php include("header.php"); ?>
+<?php include("header.php"); ?>
 
-    <main>
-        <div class="coll-en-ligne wrapper">
-            <div class="texte-coll">
-                <H1>Collections</H1>
-                <svg class="pastille-collections">
-                    <ellipse rx="5.8045854568481445" ry="5.8045854568481445" cx="5.8045854568481445" cy="5.8045854568481445">
-                    </ellipse>
-                </svg>
-                <p class="titre-intro">
-                Musée contrôlé depuis 1984, puis labellisé « Musée de France » en 
-                2003, le CAPC musée d’art contemporain a constitué une collection 
-                de 1299 œuvres de 189 artistes comprenant d’une part des achats et 
-                d’autre part des œuvres mises en dépôt par le MNAM (Musée National 
-                d’Art Moderne, Centre Georges Pompidou), par le FNAC (Fonds National 
-                d’Art Contemporain), auxquelles s’ajoutent ceux de la Caisse des Dépôts 
-                et Consignations, des dépôts d’artistes et de collectionneurs. Ils 
-                témoignent des rencontres et des échanges entre les artistes, le 
+<main>
+    <div class="coll-en-ligne wrapper">
+        <div class="texte-coll">
+            <H1>Collections</H1>
+            <svg class="pastille-collections">
+                <ellipse rx="5.8045854568481445" ry="5.8045854568481445" cx="5.8045854568481445"
+                         cy="5.8045854568481445">
+                </ellipse>
+            </svg>
+            <p class="titre-intro">
+                Musée contrôlé depuis 1984, puis labellisé « Musée de France » en
+                2003, le CAPC musée d’art contemporain a constitué une collection
+                de 1299 œuvres de 189 artistes comprenant d’une part des achats et
+                d’autre part des œuvres mises en dépôt par le MNAM (Musée National
+                d’Art Moderne, Centre Georges Pompidou), par le FNAC (Fonds National
+                d’Art Contemporain), auxquelles s’ajoutent ceux de la Caisse des Dépôts
+                et Consignations, des dépôts d’artistes et de collectionneurs. Ils
+                témoignent des rencontres et des échanges entre les artistes, le
                 lieu et les directeurs successifs du musée.
-                </p>
-                <button class="secondaire bouton" href="https://www.navigart.fr/capc/#/artworks">
-                    Consulter la collection en ligne
-                    <img src="assets/media/img/icons/fleche.svg" />
-                </button>
-            </div>
-            <div class="image-coll">
-                <img class="grille" src="assets/media/img/grille-page-coll.svg" />
-                <img class="pres" src="assets/media/img/collection-pres.svg" />
-            </div>
+            </p>
+            <a class="secondaire bouton lien-collection" href="https://www.navigart.fr/capc/#/artworks">
+                Consulter la collection en ligne
+                <img src="assets/media/img/icons/fleche.svg"/>
+            </a>
         </div>
+        <div class="image-coll">
+            <img class="grille" src="assets/media/img/grille-page-coll.svg"/>
+            <img class="pres" src="assets/media/img/collection-pres.svg"/>
+        </div>
+    </div>
 
-
-        <!-- Swiper -->
-        <div class="swiper-container">
-            <div class="swiper-wrapper">
-                <div class="swiper-slide">
-                    <h2>Les oeuvres in situ</h2>
+    <div class="coll-en-ligne wrapper">
+        <h2>Les oeuvres in situ</h2>
+    </div>
+    <!-- Swiper -->
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="flex-space-between">
                     <div class="pleine-page">
                         <span class="artiste-pleine-page">Christian Boltanski</span>
                         <div class="legende-pleine-page">Pour mémoire, 1974</div>
                         <p class="texte-pleine-page">
-                            En 2005, l’artiste choisit le CAPC comme dépositaire de documents et d’objets «lui 
-                            ayant appartenu ». S’agit-il ici d’objets personnels de l’artiste ? Nous sommes ici à 
-                            la fois dans la mythologie individuelle de l’artiste et dans son habituel principe 
-                            d’inventaire.<br />
-                            <br />
-                            Dans l’œuvre de Boltanski, traversée par les questions de mémoire et d’identité, les 
-                            individus perdus dans l’histoire collective continuent d’être présents grâce à un 
-                            nom, une modeste photo, quelques traces d’existence. Le lieu attribué à cette 
-                            œuvre est un petit local fermé, dont la porte est percée d’un fenestron dans la 
+                            En 2005, l’artiste choisit le CAPC comme dépositaire de documents et d’objets «lui
+                            ayant appartenu ». S’agit-il ici d’objets personnels de l’artiste ? Nous sommes ici à
+                            la fois dans la mythologie individuelle de l’artiste et dans son habituel principe
+                            d’inventaire.<br/>
+                            <br/>
+                            Dans l’œuvre de Boltanski, traversée par les questions de mémoire et d’identité, les
+                            individus perdus dans l’histoire collective continuent d’être présents grâce à un
+                            nom, une modeste photo, quelques traces d’existence. Le lieu attribué à cette
+                            œuvre est un petit local fermé, dont la porte est percée d’un fenestron dans la
                             volée de l’escalier nord-est de l’Entrepôt.
                         </p>
                     </div>
-                    <div class="image-pleine-page" >
-                        <img src="assets/media/img/christianboltanski.svg" />
+                    <div class="image-pleine-page">
+                        <img src="assets/media/img/christianboltanski.svg"/>
                     </div>
                 </div>
-                <div class="swiper-slide">
-                    <h2>Les oeuvres in situ</h2>
-                    <div class="pleine-page">
-                        <span class="artiste-pleine-page">Christian Boltanski</span>
-                        <div class="legende-pleine-page">Pour mémoire, 1974</div>
-                        <p class="texte-pleine-page">
-                            En 2005, l’artiste choisit le CAPC comme dépositaire de documents et d’objets «lui 
-                            ayant appartenu ». S’agit-il ici d’objets personnels de l’artiste ? Nous sommes ici à 
-                            la fois dans la mythologie individuelle de l’artiste et dans son habituel principe 
-                            d’inventaire.<br />
-                            <br />
-                            Dans l’œuvre de Boltanski, traversée par les questions de mémoire et d’identité, les 
-                            individus perdus dans l’histoire collective continuent d’être présents grâce à un 
-                            nom, une modeste photo, quelques traces d’existence. Le lieu attribué à cette 
-                            œuvre est un petit local fermé, dont la porte est percée d’un fenestron dans la 
-                            volée de l’escalier nord-est de l’Entrepôt.
-                        </p>
-                    </div>
-                    <div class="image-pleine-page" >
-                        <img src="assets/media/img/christianboltanski.svg" />
-                    </div>
-                </div>
-                
             </div>
-            <!-- Add Pagination -->
-            <div class="swiper-pagination"></div>
         </div>
-        
-    </main>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination"></div>
+    </div>
+
+</main>
 
 
-    <?php include("footer.php"); ?>
+<?php include("footer.php"); ?>
 
-    <!--IMPORTS JS-->
-    <?php include("script.php"); ?>
-    <!--/IMPORTS JS-->
+<!--IMPORTS JS-->
+<?php include("script.php"); ?>
+<!--/IMPORTS JS-->
 
-    <script>
+<script>
     var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+        direction: 'vertical',
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
     });
-  </script>
-  </body>
+</script>
+</body>
 </html>
