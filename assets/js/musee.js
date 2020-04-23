163d3b55bafa1a618954b2e7e2907fdcf0e71813
viperee <just.b.nd.l@gmail.com>
2020-04-23 23:40:20 +0200

    Fix animation firefox musee

--- assets/js/musee.js
+++ assets/js/musee.js
@@ -2,6 +2,7 @@ $().ready(() => {
     $("#timeline-descriptions-wrapper").children("div[data-description!=1973]").hide();
     $(".pastille-date").first().siblings('svg').children().addClass('grossi');
     $(".pastille-date").click(function(){
+        console.log("CLICK");
         // AFFICHER / CACHER TEXTE
         let dateAttr = $(this).attr('data-description');
         $("#timeline-descriptions-wrapper").children("div[data-description!="+dateAttr+"]").fadeOut(1000);
@@ -12,7 +13,17 @@ $().ready(() => {
 
         // EFFET SOLEIL
         $(".grossi").addClass('degrossi');
+        $(".grossi").attr('rx', 8.5);
+        $(".grossi").attr('ry', 8.5);
+        $(".grossi").attr('cx', 8.5);
+        $(".grossi").attr('cy', 8.5);
         $(".grossi").removeClass('grossi');
+
+        $(this).siblings('svg').children().attr('rx', 97);
+        $(this).siblings('svg').children().attr('ry', 97);
+        $(this).siblings('svg').children().attr('cx', 9);
+        $(this).siblings('svg').children().attr('cy', -28);
+
         $(this).siblings('svg').children().toggleClass('grossi');
         setTimeout(function(){
             $(".degrossi").removeClass('degrossi');
