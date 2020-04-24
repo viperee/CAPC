fa68d87186003275a1f2a2f3570057a2dfc3092a
viperee <just.b.nd.l@gmail.com>
2020-04-24 21:39:45 +0200

    Collections

--- collections.php
+++ collections.php
@@ -11,8 +11,8 @@
 <body>
 <?php include("header.php"); ?>
 
-<main>
-    <div class="coll-en-ligne wrapper">
+<main class="wrapper">
+    <div class="collection inline-block">
         <div class="texte-coll">
             <H1>Collections</H1>
             <svg class="pastille-collections">
@@ -42,39 +42,103 @@
         </div>
     </div>
 
-    <div class="coll-en-ligne wrapper">
+    <div class="inline-block">
         <h2>Les oeuvres in situ</h2>
     </div>
-    <!-- Swiper -->
-    <div class="swiper-container">
-        <div class="swiper-wrapper">
-            <div class="swiper-slide">
-                <div class="flex-space-between">
-                    <div class="pleine-page">
-                        <span class="artiste-pleine-page">Christian Boltanski</span>
-                        <div class="legende-pleine-page">Pour mémoire, 1974</div>
-                        <p class="texte-pleine-page">
-                            En 2005, l’artiste choisit le CAPC comme dépositaire de documents et d’objets «lui
-                            ayant appartenu ». S’agit-il ici d’objets personnels de l’artiste ? Nous sommes ici à
-                            la fois dans la mythologie individuelle de l’artiste et dans son habituel principe
-                            d’inventaire.<br/>
-                            <br/>
-                            Dans l’œuvre de Boltanski, traversée par les questions de mémoire et d’identité, les
-                            individus perdus dans l’histoire collective continuent d’être présents grâce à un
-                            nom, une modeste photo, quelques traces d’existence. Le lieu attribué à cette
-                            œuvre est un petit local fermé, dont la porte est percée d’un fenestron dans la
-                            volée de l’escalier nord-est de l’Entrepôt.
-                        </p>
-                    </div>
-                    <div class="image-pleine-page">
-                        <img src="assets/media/img/christianboltanski.svg"/>
-                    </div>
-                </div>
-            </div>
+    <div class="oeuvre flex-space-between">
+        <div class="pleine-page">
+            <span class="artiste-pleine-page">Christian Boltanski</span>
+            <div class="legende-pleine-page">Pour mémoire, 1974</div>
+            <p class="texte-pleine-page">
+                En 2005, l’artiste choisit le CAPC comme dépositaire de documents et d’objets «lui
+                ayant appartenu ». S’agit-il ici d’objets personnels de l’artiste ? Nous sommes ici à
+                la fois dans la mythologie individuelle de l’artiste et dans son habituel principe
+                d’inventaire.<br/>
+                <br/>
+                Dans l’œuvre de Boltanski, traversée par les questions de mémoire et d’identité, les
+                individus perdus dans l’histoire collective continuent d’être présents grâce à un
+                nom, une modeste photo, quelques traces d’existence. Le lieu attribué à cette
+                œuvre est un petit local fermé, dont la porte est percée d’un fenestron dans la
+                volée de l’escalier nord-est de l’Entrepôt.
+            </p>
         </div>
-        <!-- Add Pagination -->
-        <div class="swiper-pagination"></div>
+        <div class="image-pleine-page">
+            <img src="assets/media/img/christianboltanski.svg"/>
+        </div>
     </div>
+
+    <div class="oeuvre flex-space-between">
+        <div class="pleine-page">
+            <span class="artiste-pleine-page">Keith Haring</span>
+            <div class="legende-pleine-page">Untitled, 1985</div>
+            <p class="texte-pleine-page">
+                En 1985, le CAPC consacre simultanément tous ses espaces à Keith Haring : la Nef, les galeries du rez de
+                chaussée et du second étage. Après l’accrochage de ses dessins au rez de chaussée, l’artiste peint
+                directement sur les murs de la galerie. Le CAPC lui propose d’intervenir sur le mur de la cage
+                d’ascenseur qui relie les trois parties de cette exposition. Au fond de l’ascenseur, un écran vitré est
+                aménagé à la manière d’une fenêtre. La silhouette d’un homme s’étire au long du parcours sur les deux
+                étages. Tout en haut on découvre sa tête, auréolée de petits traits, à la manière de L’enfant radieux,
+                une figure récurrente du travail de l’artiste.
+            </p>
+        </div>
+        <div class="image-pleine-page">
+            <img src="assets/media/img/keith_haring.svg"/>
+        </div>
+    </div>
+
+    <div class="oeuvre flex-space-between">
+        <div class="pleine-page">
+            <span class="artiste-pleine-page">Richard Long</span>
+            <div class="legende-pleine-page">White Rock Line, 1990</div>
+            <p class="texte-pleine-page">
+                Cette ligne de pierres réalisée sur place a été commandée au sculpteur à
+                l’occasion de la dernière réhabilitation du musée en 1990. C’est une
+                sculpture de 18 tonnes composée de moellons, disposés au sol selon un
+                rectangle prédéterminé. La pierre calcaire claire provient des carrières de
+                Bourg-sur-Gironde. C’est la même pierre que celle utilisée pour la
+                construction de l’Entrepôt et cette citation du site est délibérée.
+            </p>
+        </div>
+        <div class="image-pleine-page">
+            <img src="assets/media/img/richard_long1.svg"/>
+        </div>
+    </div>
+
+    <div class="oeuvre flex-space-between">
+        <div class="pleine-page">
+            <span class="artiste-pleine-page">Richard Long</span>
+            <div class="legende-pleine-page">WGaronne Mud Circles, 1990</div>
+            <p class="texte-pleine-page">
+                Les deux cercles de boue qui se font face sur deux murs du Café du musée
+                ont été réalisés par Richard Long au printemps 1990, à la fin du chantier de
+                rénovation de l’Entrepôt Lainé. L’un est composé de quatre cercles
+                concentriques en boue directement appliquée sur la cimaise, l’autre d’un
+                seul cercle, plus large, contrastant sur un fond de peinture noire.
+            </p>
+        </div>
+        <div class="image-pleine-page">
+            <img src="assets/media/img/richard_long2.svg"/>
+        </div>
+    </div>
+
+    <div class="oeuvre flex-space-between">
+        <div class="pleine-page">
+            <span class="artiste-pleine-page">Niele Toroni</span>
+            <div class="legende-pleine-page">Empreintes, 1997-1998</div>
+            <p class="texte-pleine-page">
+                Deux œuvres sont présentées. Au rez-de-chaussée, l’œuvre a été peinte sur la paroi de béton, au-dessus
+                de la porte de l’ascenseur ; l’autre œuvre, au second étage, est un tondo fixé au mur. Celui-ci a été
+                réalisé sur un couvercle de barrique de vin. Avec leurs alignements d’empreintes de pinceau n°50,
+                espacées régulièrement tous les 30 centimètres, ces deux œuvres se répondent et suggèrent une
+                signalétique : chacune forme un triangle, pointe tournée vers le haut pour celle du rez-de-chaussée et
+                pointe tournée vers le bas pour celle de l’étage.
+            </p>
+        </div>
+        <div class="image-pleine-page">
+            <img src="assets/media/img/niele_toroni.svg"/>
+        </div>
+    </div>
+
 
 </main>
 
@@ -85,14 +149,5 @@
 <?php include("script.php"); ?>
 <!--/IMPORTS JS-->
 
-<script>
-    var swiper = new Swiper('.swiper-container', {
-        direction: 'vertical',
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-    });
-</script>
 </body>
 </html>
